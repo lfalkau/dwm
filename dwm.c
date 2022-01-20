5d398d3ef3e819749d9bb79324772226fe43b02f
Loic Buckwell <lfalkau@student.42.fr>
2022-01-20 01:39:13 +0100

    Boldize the font to indicate that there's a window in a tag / instead of drawing a square

--- dwm.c
+++ dwm.c
@@ -721,10 +721,15 @@ drawbar(Monitor *m)
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], 0);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i, 0);
+		if (occ & 1 << i) {
+      Fnt *save = drw->fonts;
+      if (save)
+        drw->fonts = drw->fonts->next;
+      drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], 0);
+      drw->fonts = save;
+    } else {
+      drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], 0);
+    }
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
